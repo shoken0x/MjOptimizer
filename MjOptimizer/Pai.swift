6f13d3474fac0cd412ed0285ff045e787f868d4e
fetaro <fetaro@gmail.com>
2014-06-16 01:23:11 +0900

    牌文字列からPaiの配列を作るユーティリティメソッド追加

--- MjOptimizer/Pai.swift
+++ MjOptimizer/Pai.swift
@@ -28,26 +28,53 @@ class Pai {
     var number: Int
     var direction: PaiDirection
     
+    // "m1tm2tm3t"といった牌の配列文字列をPaiの配列にする
+    class func stringToPaiList(paiListStr : String)->Pai[]{
+        var strList: String[] = []
+        for c in paiListStr {
+            var tmp:String = "" + c //charactorをstringにする
+            strList.append(tmp)
+        }
+        if ((strList.count % 3) != 0) {
+            println("[エラー] paiListStrの長さが３の倍数ではありません")
+            return []
+            //TODO 例外を投げる
+        }
+        
+        var paiList: Pai[] = []
+        for i in 0...strList.count {
+            if i % 3 == 2 {
+                paiList.append(Pai(paiStr: strList[i-2] + strList[i-1] + strList[i-0]))
+            }
+        }
+        return paiList
+    }
+    
+    //種類と数字から作るコンストラクタ
     init(type: PaiType,number: Int){
         self.type = type
         self.number = number
         self.direction = PaiDirection.TOP
     }
     
+    //種類と数字と向きから作るコンストラクタ
     init(type: PaiType,number: Int,direction: PaiDirection){
         self.type = type
         self.number = number
         self.direction = direction
     }
     
-    //"m1t"などの文字列からオブジェクトを作る
+    //"m1t"などの文字列から作るコンストラクタ
+    //"m1 "のように、３文字目を空白スペースにした場合はTOPで作られる
     init(paiStr: String){
-        var _ary: String[] = []
+        //Stringを「長さ１のString」の配列に変換
+        var strList: String[] = []
         for c in paiStr {
-            var tmp:String = "" + c //convert c from charactor to string
-            _ary.append(tmp)
+            var tmp:String = "" + c //charactorをstringにする
+            strList.append(tmp)
         }
-        switch _ary[0] {
+        
+        switch strList[0] {
         case "m":
             self.type = PaiType.MANZU
         case "s":
@@ -58,11 +85,11 @@ class Pai {
             self.type = PaiType.JIHAI
         default:
             self.type = PaiType.MANZU
-            println("[MjOptimizer ERROR] Invalied paiType¥(_ary[0])")
+            println("[エラー] 不正なpaiType¥(strList[0])")
             //TODO 例外を投げる
         }
-        self.number = _ary[1].toInt()!
-        switch _ary[2] {
+        self.number = strList[1].toInt()!
+        switch strList[2] {
         case "t":
             self.direction = PaiDirection.TOP
         case "b":
@@ -71,23 +98,33 @@ class Pai {
             self.direction = PaiDirection.RIGHT
         case "l":
             self.direction = PaiDirection.LEFT
+        case " ":
+            self.direction = PaiDirection.TOP
         default:
             self.direction = PaiDirection.TOP
-            println("[MjOptimizer ERROR] Invalied paiDirection¥(_ary[2])")
+            println("[エラー] 不正なpaiDirection¥(strList[2])")
             //TODO 例外を投げる
         }
     }
+    
+    // getter and setter
     func getType() -> PaiType{return self.type}
     func getNumber() -> Int{return self.number}
     func getDirection() -> PaiDirection{return self.direction}
+    
+    // 種類をStringで返す
     func getTypeStr() -> String{
         let typeStrMap = [PaiType.MANZU:"m",PaiType.SOUZU:"s",PaiType.PINZU:"p",PaiType.JIHAI:"j"]
         return typeStrMap[self.type]!
     }
+    
+    // 向きをStringで返す
     func getDirectionStr() -> String{
         let directionStrMap = [PaiDirection.TOP:"t",PaiDirection.BOTTOM:"b",PaiDirection.RIGHT:"r",PaiDirection.LEFT:"l"]
        return directionStrMap[self.direction]!
     }
+    
+    // "m1t"といった牌を表す文字列で返す
     func toString() -> String{
         return getTypeStr() + String(self.number) + getDirectionStr()
     }
--- MjOptimizerTests/PaiSpec.swift
+++ MjOptimizerTests/PaiSpec.swift
@@ -11,9 +11,9 @@ import Quick
 
 class PaiSpec: QuickSpec {
     override func exampleGroups() {
-        describe("Pai Class") {
+        describe("init") {
             beforeEach { }
-            it("init1") {
+            it("1") {
                 var pai:Pai = Pai(type: PaiType.MANZU,number: 1,direction: PaiDirection.LEFT)
                 expect(pai.type == PaiType.MANZU).to.beTrue()
                 expect(pai.getType() == PaiType.MANZU).to.beTrue()
@@ -28,7 +28,7 @@ class PaiSpec: QuickSpec {
                 
                 expect(pai.toString()).to.equal("m1l")
             }
-            it("init2") {
+            it("2") {
                 var pai:Pai = Pai(type: PaiType.MANZU,number: 1)
                 expect(pai.type == PaiType.MANZU).to.beTrue()
                 expect(pai.getType() == PaiType.MANZU).to.beTrue()
@@ -43,8 +43,24 @@ class PaiSpec: QuickSpec {
                 
                 expect(pai.toString()).to.equal("m1t")
             }
-            it("init3") {
-                var pai:Pai = Pai(paiStr: "m1t")
+            it("3") {
+                var pai:Pai = Pai(paiStr: "m1l")
+                
+                expect(pai.type == PaiType.MANZU).to.beTrue()
+                expect(pai.getType() == PaiType.MANZU).to.beTrue()
+                expect(pai.getTypeStr()).to.equal("m")
+                
+                expect(pai.number).to.equal(1)
+                expect(pai.getNumber()).to.equal(1)
+                
+                expect(pai.direction == PaiDirection.LEFT).to.beTrue()
+                expect(pai.getDirection() == PaiDirection.LEFT).to.beTrue()
+                expect(pai.getDirectionStr()).to.equal("l")
+                
+                expect(pai.toString()).to.equal("m1l")
+            }
+            it("4") {
+                var pai:Pai = Pai(paiStr: "m1 ")
                 
                 expect(pai.type == PaiType.MANZU).to.beTrue()
                 expect(pai.getType() == PaiType.MANZU).to.beTrue()
@@ -58,6 +74,17 @@ class PaiSpec: QuickSpec {
                 expect(pai.getDirectionStr()).to.equal("t")
                 
                 expect(pai.toString()).to.equal("m1t")
+            }
+        }
+        describe("stringToPaiList"){
+            it("1"){
+                var paiList:Pai[] = Pai.stringToPaiList("s2lm1 p5t")
+                expect(paiList[0].toString()).to.equal("s2l")
+                expect(paiList[1].toString()).to.equal("m1t")
+                expect(paiList[2].toString()).to.equal("p5t")
+            }
+            it("2"){
+                var paiList:Pai[] = Pai.stringToPaiList("s")
             }
         }
     }
