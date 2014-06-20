99a4ef3312cac360bd97e1a849c0cdae97dfe768
bisque33 <bisque33@gmail.com>
2014-06-21 01:34:37 +0900

    五十嵐のソースをマージ

--- MjOptimizer/SutehaiSelectResult.swift
+++ MjOptimizer/SutehaiSelectResult.swift
@@ -9,13 +9,30 @@
 import Foundation
 
 class SutehaiSelectResult {
+<<<<<<< HEAD
     var sutehaiCandidateList : SutehaiCandidate[]
     var tehaiShantenNum : Int
     
     init(sutehaiCandidateList : SutehaiCandidate[],tehaiShantenNum : Int){
+=======
+    var sutehaiCandidateList : SutehaiCandidate[] //シャンテ数
+    var tehaiShantenNum : Int //シャンテン数
+    var tehai : Pai[] //手牌
+    var isFinishAnalyze : Bool //解析に成功したか
+    var successNum : Int //解析に成功した枚数
+    
+    init(sutehaiCandidateList : SutehaiCandidate[],tehaiShantenNum : Int,tehai : Pai[],isFinishAnalyze : Bool,successNum : Int){
+>>>>>>> f984585add9706747e77859a0c96fefe2c034026
         self.sutehaiCandidateList = sutehaiCandidateList
         self.tehaiShantenNum = tehaiShantenNum
+        self.tehai = tehai
+        self.isFinishAnalyze = isFinishAnalyze
+        self.successNum = successNum
+        
     }
     func getSutehaiCandidateList() -> SutehaiCandidate[]{return self.sutehaiCandidateList}
     func getTehaiShantenNum() -> Int{return self.tehaiShantenNum}
+    func getTehai() -> Pai[]{return self.tehai}
+    func getSuccessNum() -> Int{return self.successNum}
+    
 }
